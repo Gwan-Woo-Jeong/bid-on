0cdb5461f6d1cc35d86b521db64b6d6af8025a75
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2024-11-20 23:32:10 +0900

    init

---
+++ src/main/java/com/test/bidon/BidOnApplication.java
@@ -0,0 +1,13 @@
+package com.test.bidon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/bidon/BidOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.bidon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
