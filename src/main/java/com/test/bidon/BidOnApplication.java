188e40c351801ed089126e3457dec88824c61920
jinliuli <jinliuli0305@gmail.com>
2024-11-22 19:34:17 +0900

    Chore: admin 템플릿 및 조각페이지 추가

--- src/main/java/com/test/bidon/BidOnApplication.java
+++ src/main/java/com/test/bidon/BidOnApplication.java
@@ -1,6 +1,6 @@
 package com.test.bidon;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BidOnApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//        Dotenv dotenv = Dotenv.load();
+//
+//        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(BidOnApplication.class, args);
     }
