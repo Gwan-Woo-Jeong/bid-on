b2295da3731dc70475f9a3a14170d740d83d3e16
hyemilaptop <gpal4180@naver.com>
2024-11-29 09:30:15 +0900

    Chore: 우진이가 건든 로그인 버튼 수정

--- src/main/generated/com/test/bidon/entity/QNormalAuctionItem.java
+++ src/main/generated/com/test/bidon/entity/QNormalAuctionItem.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,36 +18,49 @@ public class QNormalAuctionItem extends EntityPathBase<NormalAuctionItem> {
 
     private static final long serialVersionUID = -11221979L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QNormalAuctionItem normalAuctionItem = new QNormalAuctionItem("normalAuctionItem");
 
     public final NumberPath<Long> categorySubId = createNumber("categorySubId", Long.class);
 
     public final StringPath description = createString("description");
 
-    public final StringPath endTime = createString("endTime");
+    public final DateTimePath<java.time.LocalDateTime> endTime = createDateTime("endTime", java.time.LocalDateTime.class);
 
     public final NumberPath<Long> id = createNumber("id", Long.class);
 
     public final StringPath name = createString("name");
 
-    public final StringPath startPrice = createString("startPrice");
+    public final NumberPath<Integer> startPrice = createNumber("startPrice", Integer.class);
 
-    public final StringPath startTime = createString("startTime");
+    public final DateTimePath<java.time.LocalDateTime> startTime = createDateTime("startTime", java.time.LocalDateTime.class);
 
     public final StringPath status = createString("status");
+
+    public final QUserEntity userInfo;
 
     public final NumberPath<Long> userInfoId = createNumber("userInfoId", Long.class);
 
     public QNormalAuctionItem(String variable) {
-        super(NormalAuctionItem.class, forVariable(variable));
+        this(NormalAuctionItem.class, forVariable(variable), INITS);
     }
 
     public QNormalAuctionItem(Path<? extends NormalAuctionItem> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QNormalAuctionItem(PathMetadata metadata) {
-        super(NormalAuctionItem.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QNormalAuctionItem(PathMetadata metadata, PathInits inits) {
+        this(NormalAuctionItem.class, metadata, inits);
+    }
+
+    public QNormalAuctionItem(Class<? extends NormalAuctionItem> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.userInfo = inits.isInitialized("userInfo") ? new QUserEntity(forProperty("userInfo")) : null;
     }
 
 }
