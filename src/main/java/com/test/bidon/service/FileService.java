140b2a5c08d7cc7327e9f8f477413d01da578494
hyemilaptop <gpal4180@naver.com>
2024-11-29 14:17:42 +0900

    회원가입 프로필 이미지 등록 및 개인정보 프로필 이미지 수정 기능 추가

--- src/main/java/com/test/bidon/service/FileService.java
+++ src/main/java/com/test/bidon/service/FileService.java
@@ -2,14 +2,12 @@ package com.test.bidon.service;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path; // java.nio.file.Path 사용
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-//FileService.java
 @Service
 public class FileService {
  private static final String UPLOAD_DIR = "C:/class/code/springBoot/bid-on/src/main/resources/static/user/images/review";
@@ -21,9 +19,9 @@ public class FileService {
 
      try {
          // 고유한 파일 이름 생성
-         String uniqueFileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-         Path filePath = Paths.get(UPLOAD_DIR, uniqueFileName);
-
+         String fileName = file.getOriginalFilename();
+         Path filePath = Paths.get(UPLOAD_DIR, fileName);
+         
          // 디렉토리 생성
          Files.createDirectories(filePath.getParent());
 
