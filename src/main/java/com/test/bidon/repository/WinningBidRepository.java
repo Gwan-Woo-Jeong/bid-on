91230097c5d902ef0d9d12ee792bc98a0013b462
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2024-12-03 10:45:19 +0900

    Fix: WinningBidRepository param 어노테이션 명시

--- src/main/java/com/test/bidon/repository/WinningBidRepository.java
+++ src/main/java/com/test/bidon/repository/WinningBidRepository.java
@@ -15,36 +15,37 @@ public interface WinningBidRepository extends JpaRepository<WinningBid, Long> {
 
     // 실시간 경매 ID와 최종 낙찰 ID로 실제 존재 여부 확인
     @Query("SELECT COUNT(*) FROM WinningBid wb JOIN LiveBidCost lc ON wb.liveBidId = lc.id WHERE wb.id = :winningBidId AND lc.liveAuctionItemId = :liveAuctionItemId AND wb.userInfoId = :userId")
-    int countByWinBidIdAndLiveItemId(@Param("winningBidId") Long winningBidId, @Param("liveAuctionItemId") Long liveAuctionItemId, Long userId);
+    int countByWinBidIdAndLiveItemId(@Param("winningBidId") Long winningBidId, @Param("liveAuctionItemId") Long liveAuctionItemId, @Param("userId") Long userId);
 
     // 일반 경매 ID와 최종 낙찰 ID로 실제 존재 여부 확인
     @Query("SELECT COUNT(*) FROM WinningBid wb JOIN NormalBidInfo nb ON wb.normalBidId = nb.id WHERE wb.id = :winningBidId AND nb.auctionItemId = :auctionItemId AND wb.userInfoId = :userId")
-    int countByWinBidIdAndNormalItemId(@Param("winningBidId") Long winningBidId, @Param("auctionItemId") Long auctionItemId, Long userId);
+    int countByWinBidIdAndNormalItemId(@Param("winningBidId") Long winningBidId, @Param("auctionItemId") Long auctionItemId, @Param("userId") Long userId);
 
-	// 사용자 ID와 일반 경매 ID로 낙찰 정보 찾기
+    // 사용자 ID와 일반 경매 ID로 낙찰 정보 찾기
     Optional<WinningBid> findByUserInfoIdAndNormalBidIdIsNotNull(Long userInfoId);
-    
+
     // 사용자 ID와 실시간 경매 ID로 낙찰 정보 찾기
     List<WinningBid> findByUserInfoIdAndLiveBidIdIsNotNull(Long userInfoId);
-    
+
     // 일반 경매 ID로 낙찰 정보 찾기
     Optional<WinningBid> findByNormalBidId(Long normalBidId);
-    
+
     // 실시간 경매 ID로 낙찰 정보 찾기
     Optional<WinningBid> findByLiveBidId(Long liveBidId);
-    
+
     // 일반 경매 낙찰 목록
     List<WinningBid> findByNormalBidIdIsNotNull();
-    
+
     // 실시간 경매 낙찰 목록
     List<WinningBid> findByLiveBidIdIsNotNull();
 
-	int countByUserInfoIdAndNormalBidIdIsNotNull(Long userInfoId);
+    int countByUserInfoIdAndNormalBidIdIsNotNull(Long userInfoId);
 
-	int countByUserInfoIdAndLiveBidIdIsNotNull(Long userInfoId);
+    int countByUserInfoIdAndLiveBidIdIsNotNull(Long userInfoId);
 
     // WinningBidRepository에 추가할 메서드
     List<WinningBid> findAllByUserInfoIdAndNormalBidIdIsNotNull(Long userInfoId);
+
     List<WinningBid> findAllByUserInfoIdAndLiveBidIdIsNotNull(Long userInfoId);
-    
+
 }
